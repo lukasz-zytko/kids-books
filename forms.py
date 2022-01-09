a5cfb0da15b2b612d14bbb753cf9120104af1bb3
Lukasz Zytko <zytko.lukasz@gmail.com>
2022-01-09 21:03:06 +0100

    Add new forms

--- forms.py
+++ forms.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, BooleanField, IntegerField
+from wtforms import StringField, TextAreaField, BooleanField, IntegerField, SelectField
 from wtforms.validators import DataRequired, Email
 
 class BooksForm(FlaskForm):
     title = StringField('Tytuł', validators=[DataRequired()])
     author = StringField('Autor', validators=[DataRequired()])
     pages = IntegerField('Liczba stron', validators=[DataRequired()])
+    age = SelectField('Kategoria wiekowa', choices=['0-3', '4-6', '7-10', 'all'], validators=[DataRequired()])
